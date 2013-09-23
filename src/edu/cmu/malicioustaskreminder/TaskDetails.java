ac2391a3c56964d37a36bf0e9952498061bfb95f
Venkatesh Sriram <s.venkatesh.5789@gmail.com>
2013-09-23 09:43:22 -0700

    made the UI of the task details page, with large text views, buttons and time picker. Need to make the date picker

--- src/edu/cmu/malicioustaskreminder/TaskDetails.java
+++ src/edu/cmu/malicioustaskreminder/TaskDetails.java
@@ -2,10 +2,25 @@ package edu.cmu.malicioustaskreminder;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class TaskDetails extends Activity {
-
+	
+	private TextView tvDisplayDate;
+	private DatePicker dpResult;
+	private Button btnChangeDate;
+ 
+	private int year;
+	private int month;
+	private int day;
+ 
+	static final int DATE_DIALOG_ID = 999;
+ 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,5 +33,76 @@ public class TaskDetails extends Activity {
 		getMenuInflater().inflate(R.menu.task_details, menu);
 		return true;
 	}
+	
+	// display current date
+		public void setCurrentDateOnView() {
+	 
+			tvDisplayDate = (TextView) findViewById(R.id.tvDate);
+			dpResult = (DatePicker) findViewById(R.id.dpResult);
+	 
+			final Calendar c = Calendar.getInstance();
+			year = c.get(Calendar.YEAR);
+			month = c.get(Calendar.MONTH);
+			day = c.get(Calendar.DAY_OF_MONTH);
+	 
+			// set current date into textview
+			tvDisplayDate.setText(new StringBuilder()
+				// Month is 0 based, just add 1
+				.append(month + 1).append("-").append(day).append("-")
+				.append(year).append(" "));
+	 
+			// set current date into datepicker
+			dpResult.init(year, month, day, null);
+	 
+		}
+	 
+		public void addListenerOnButton() {
+	 
+			btnChangeDate = (Button) findViewById(R.id.btnChangeDate);
+	 
+			btnChangeDate.setOnClickListener(new OnClickListener() {
+	 
+				@Override
+				public void onClick(View v) {
+	 
+					showDialog(DATE_DIALOG_ID);
+	 
+				}
+	 
+			});
+	 
+		}
+	 
+		@Override
+		protected Dialog onCreateDialog(int id) {
+			switch (id) {
+			case DATE_DIALOG_ID:
+			   // set date picker as current date
+			   return new DatePickerDialog(this, datePickerListener, 
+	                         year, month,day);
+			}
+			return null;
+		}
+	 
+		private DatePickerDialog.OnDateSetListener datePickerListener 
+	                = new DatePickerDialog.OnDateSetListener() {
+	 
+			// when dialog box is closed, below method will be called.
+			public void onDateSet(DatePicker view, int selectedYear,
+					int selectedMonth, int selectedDay) {
+				year = selectedYear;
+				month = selectedMonth;
+				day = selectedDay;
+	 
+				// set selected date into textview
+				tvDisplayDate.setText(new StringBuilder().append(month + 1)
+				   .append("-").append(day).append("-").append(year)
+				   .append(" "));
+	 
+				// set selected date into datepicker also
+				dpResult.init(year, month, day, null);
+	 
+			}
+		};
 
 }
