6739972ebb814cbbf722a2b2e913edf30d5814ef
Ramya B <ramya201@gmail.com>
2013-09-30 16:57:02 -0700

    Changes to R.java

--- gen/edu/cmu/malicioustaskreminder/R.java
+++ gen/edu/cmu/malicioustaskreminder/R.java
@@ -21,24 +21,28 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int btnbackgrnd=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int listbckgrnd=0x7f020002;
     }
     public static final class id {
-        public static final int action_home=0x7f08000f;
+        public static final int action_home=0x7f080011;
         public static final int addTasksButton=0x7f080000;
-        public static final int allTasksListView=0x7f08000b;
-        public static final int buttonAddTask=0x7f08000a;
-        public static final int buttonEmailFriend=0x7f08000c;
+        public static final int allTasksListView=0x7f08000c;
+        public static final int buttonAddTask=0x7f08000b;
+        public static final int buttonEmailFriend=0x7f080010;
         public static final int emailText=0x7f08000e;
-        public static final int friendEmail=0x7f08000d;
-        public static final int fromTime=0x7f080004;
-        public static final int fromTimeText=0x7f080007;
-        public static final int selectedDateText=0x7f080008;
-        public static final int taskDate=0x7f080009;
-        public static final int taskDescription=0x7f080005;
-        public static final int taskDescriptionText=0x7f080006;
-        public static final int toTime=0x7f080002;
-        public static final int toTimeText=0x7f080003;
+        public static final int friendEmail=0x7f08000f;
+        public static final int fromTime=0x7f080006;
+        public static final int fromTimeText=0x7f080005;
+        public static final int linearLayout1=0x7f080002;
+        public static final int mainLayout=0x7f08000d;
+        public static final int selectedDateText=0x7f080009;
+        public static final int taskDate=0x7f08000a;
+        public static final int taskDescription=0x7f080004;
+        public static final int taskDescriptionText=0x7f080003;
+        public static final int toTime=0x7f080008;
+        public static final int toTimeText=0x7f080007;
         public static final int viewTasksButton=0x7f080001;
     }
     public static final class layout {
@@ -98,5 +102,7 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int TextFont=0x7f060003;
+        public static final int TitleFont=0x7f060002;
     }
 }
